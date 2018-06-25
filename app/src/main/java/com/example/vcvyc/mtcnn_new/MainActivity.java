f3197f2f3909bcc33a01db5c34a54b173f6c15d6
vcvycy <352871242@qq.com>
2018-06-25 18:03:17 +0800

    code

---
+++ app/src/main/java/com/example/vcvyc/mtcnn_new/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.vcvyc.mtcnn_new;
+/*
+  MTCNN For Android
+  by cjf@xmu 20180625
+ */
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG="MainActivity";
+    private  Bitmap readFromAssets(String filename){
+        Bitmap bitmap;
+        AssetManager asm=getAssets();
+        try {
+            InputStream is=asm.open(filename);
+            bitmap= BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            Log.e("MainActivity","[*]failed to open "+filename);
+            e.printStackTrace();
+            return null;
+        }
+        return Utils.copyBitmap(bitmap); //返回mutable的image
+    }
+    public void showImage(Bitmap bm,int rid){
+        ((ImageView)findViewById(rid)).setImageBitmap(bm);
+        Log.i(TAG,"[*]showImage width:"+bm.getWidth()+" Height:"+bm.getHeight());
+    }
+    public void showPixel(int v){
+        Log.i(TAG,"[*]Pixel:R"+((v>>16)&0xff)+"G:"+((v>>8)&0xff)+ " B:"+(v&0xff));
+    }
+    public void myMain(){
+        String TAG="MainActivity";
+        MTCNN mtcnn=new MTCNN(getAssets());
+        Bitmap bitmap=readFromAssets("2.jpg");
+        /*
+        try {
+            int[] tmp=new int[bitmap.getWidth()*bitmap.getHeight()];
+            bitmap.getPixels(tmp,0,bitmap.getWidth(),0,0,bitmap.getWidth(),bitmap.getHeight());
+            showPixel(tmp[bitmap.getWidth()*50+100]);
+        }catch (Exception e){
+            Log.i(TAG,"[*]exception:"+e);
+        }*/
+        //Bitmap bitmap=readFromAssets("ttt.jpg");
+        try {
+            Rect[]rects=mtcnn.detectFaces(bitmap,35);
+            Utils.drawRects(bitmap,rects);
+            showImage(bitmap,R.id.imageView);
+            if (mtcnn.tmp_bm!=null)
+                showImage(mtcnn.tmp_bm,R.id.imageView2);
+        }catch (Exception e){
+            Log.e(TAG,"[*]detect false:"+e);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myMain();
+    }
+}
---
+++ app/src/main/java/com/example/vcvyc/mtcnn_new/Utils.java
@@ -0,0 +1,96 @@
+package com.example.vcvyc.mtcnn_new;
+/*
+  MTCNN For Android
+  by cjf@xmu 20180625
+ */
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.util.Vector;
+
+public class Utils {
+    public static Bitmap copyBitmap(Bitmap bitmap){
+        return bitmap.copy(bitmap.getConfig(),true);
+    }
+    public static void drawRect(Bitmap bitmap,Rect rect){
+        try {
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            int r=(int)(Math.random()*255);
+            int g=(int)(Math.random()*255);
+            int b=(int)(Math.random()*255);
+            paint.setColor(Color.rgb(r, g, b));
+            paint.setStrokeWidth(3);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(rect, paint);
+        }catch (Exception e){
+            Log.i("Utils","[*] error"+e);
+        }
+    }
+    public static void drawRects(Bitmap bitmap, Rect[] rects){
+        for (int i=0;i<rects.length;i++)
+            drawRect(bitmap,rects[i]);
+    }
+    //Flip alone diagonal
+    //对角线翻转。data大小原先为h*w*stride，翻转后变成w*h*stride
+    public static void flip_diag(float[]data,int h,int w,int stride){
+        float[] tmp=new float[w*h*stride];
+        for (int i=0;i<w*h*stride;i++) tmp[i]=data[i];
+        for (int y=0;y<h;y++)
+            for (int x=0;x<w;x++){
+               for (int z=0;z<stride;z++)
+                 data[(x*h+y)*stride+z]=tmp[(y*w+x)*stride+z];
+            }
+    }
+    //src转为二维存放到dst中
+    public static void expand(float[] src,float[][]dst){
+        int idx=0;
+        for (int y=0;y<dst.length;y++)
+            for (int x=0;x<dst[0].length;x++)
+                dst[y][x]=src[idx++];
+    }
+    //src转为三维存放到dst中
+    public static void expand(float[] src,float[][][] dst){
+        int idx=0;
+        for (int y=0;y<dst.length;y++)
+            for (int x=0;x<dst[0].length;x++)
+                for (int c=0;c<dst[0][0].length;c++)
+                dst[y][x][c]=src[idx++];
+
+    }
+    //dst=src[:,:,1]
+    public static void expandProb(float[] src,float[][]dst){
+        int idx=0;
+        for (int y=0;y<dst.length;y++)
+            for (int x=0;x<dst[0].length;x++)
+                dst[y][x]=src[idx++*2+1];
+    }
+    //box转化为rect
+    public static Rect[] boxes2rects(Vector<Box> boxes){
+        int cnt=0;
+        for (int i=0;i<boxes.size();i++) if (!boxes.get(i).deleted) cnt++;
+        Rect[] r=new Rect[cnt];
+        int idx=0;
+        for (int i=0;i<boxes.size();i++)
+            if (!boxes.get(i).deleted)
+                r[idx++]=boxes.get(i).transform2Rect();
+        return r;
+    }
+    //删除做了delete标记的box
+    public static Vector<Box> updateBoxes(Vector<Box> boxes){
+        Vector<Box> b=new Vector<Box>();
+        for (int i=0;i<boxes.size();i++)
+            if (!boxes.get(i).deleted)
+                b.addElement(boxes.get(i));
+        return b;
+    }
+    //
+    static public void showPixel(int v){
+        Log.i("MainActivity","[*]Pixel:R"+((v>>16)&0xff)+"G:"+((v>>8)&0xff)+ " B:"+(v&0xff));
+    }
+}
